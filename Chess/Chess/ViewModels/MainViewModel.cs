ab5b2bf23e8ed57065178fc9fe9f6d4c99a70e99
Mattias Lantz Cronqvist <mattias@cronqvist.eu>
2014-11-04 17:22:05 +0100

    asd

--- Chess/Chess/ViewModels/MainViewModel.cs
+++ Chess/Chess/ViewModels/MainViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Chess.ViewModels
 {
     public class MainViewModel
     {
+        public ICommand PressedButton { get; private set; }
+
+        public MainViewModel()
+        {
+            PressedButton = new SimpleCommand(ExecutePressedButton, CanExecutePressedButton);
+        }
+
+        private bool CanExecutePressedButton(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecutePressedButton(object parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
