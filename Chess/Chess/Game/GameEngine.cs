8d49f02de55306bf0eb4be6f76d1b312110969ab
Mattias Lantz Cronqvist <mattias@cronqvist.eu>
2014-11-27 03:25:07 +0100

    removed shit references

--- Chess/Chess/Game/GameEngine.cs
+++ Chess/Chess/Game/GameEngine.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Xml.Linq;
 using Chess.Model;
 
 namespace Chess.Game
